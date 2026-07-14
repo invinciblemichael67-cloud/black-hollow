2e904b2e6979171dbe0b063c3e306833f8981d94
invinciblemichael67-cloud <invinciblemichael67@gmail.com>
2026-07-14 09:34:49 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
+const startButton = document.getElementById("startButton");
 
+const titleScreen = document.getElementById("titleScreen");
+
+const gameScreen = document.getElementById("gameScreen");
+
+const message = document.getElementById("messageBox");
+
+startButton.onclick = function(){
+
+titleScreen.classList.add("hidden");
+
+gameScreen.classList.remove("hidden");
+
+}
+
+function inspectDesk(){
+
+message.innerHTML="You search the dusty desk and find an old key.";
+
+}
+
+function inspectCell(){
+
+message.innerHTML="Cell 7 is stained with dried blood. The words 'HE NEVER LEFT' are scratched into the wall.";
+
+}
+
+function inspectDoor(){
+
+message.innerHTML="The heavy iron door won't budge.";
+
+}
